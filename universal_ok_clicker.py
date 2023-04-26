8075d668497c6917f5108e68d6331d71ddaecfbc
-= BURNED =- <54276184+Megalounge@users.noreply.github.com>
2023-04-26 16:32:46 +0800

    Update universal_ok_clicker.py

--- universal_ok_clicker.py
+++ universal_ok_clicker.py
@@ -14,6 +14,13 @@ def takess(x, y):
         active_window = pgw.getActiveWindow()
         im1 = pag.screenshot(region=(active_window.left, active_window.top, active_window.width, active_window.height))
         im1.save(r"./errors/" + fname)
+        
+        btn_reconnect = pag.locateOnScreen('reconnect.png', region=(active_window.left, active_window.top, active_window.width, active_window.height), confidence=0.8)
+        if btn_reconnect != None:
+            reconnect_x, reconnect_y = pag.center(reconnect_btn)
+            pag.leftClick(reconnect_x, reconnect_y)
+            
+    time.sleep(0.5)
 
 
 while True:
@@ -44,12 +51,14 @@ while True:
             btn_x, btn_y = pag.center(btn_confirm)
             pag.leftClick(btn_x, btn_y)
             cnt += 1
+            takess(x, y)
             i += 1
             time.sleep(0.05)
         elif btn_confirm_blue != None:
             btn_x, btn_y = pag.center(btn_confirm_blue)
             pag.leftClick(btn_x, btn_y)
             cnt += 1
+            takess(x, y)
             i += 1
             time.sleep(0.05)
 
