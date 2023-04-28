5ee753f5ca2a0ac3cf952e007978d2ec6327e4f2
-= BURNED =- <54276184+Megalounge@users.noreply.github.com>
2023-04-28 23:10:16 +0800

    Add files via upload

---
+++ required.bat
@@ -0,0 +1,7 @@
+@echo off
+
+pip install pywin32
+pip install keyboard
+pip install pyautogui
+pip install opencv-python
+pip install Pillow
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+
+python universal_ok_clicker.py
+pause
