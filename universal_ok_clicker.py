0ae0239347008c7a39d31d49b90eb17705c5d62b
-= BURNED =- <54276184+Megalounge@users.noreply.github.com>
2023-04-29 17:24:12 +0800

    Update universal_ok_clicker.py

--- universal_ok_clicker.py
+++ universal_ok_clicker.py
@@ -36,14 +36,18 @@ while True:
                 CSWindows.append(Window)
         except pgw.PyGetWindowException:
                 print(f'[{datetime.now().strftime("%H:%M:%S")}] Caught error! Please check your CSGO status.')
-                CSWindows = [w for w in CSWindows if w32p.GetWindowThreadProcessId(w)[1] != Window.pid]
+                if Window in CSWindows:
+                    CSWindows.remove(Window)
+
     
     for Window in CSWindows:
         try:
             x, y = Window.center[0] + 40, Window.center[1] + 8
         except pgw.PyGetWindowException:
             print(f'[{datetime.now().strftime("%H:%M:%S")}] Caught error! Please check your CSGO status.')
-            CSWindows = [w for w in CSWindows if w32p.GetWindowThreadProcessId(w)[1] != Window.pid]
+            if Window in CSWindows:
+                CSWindows.remove(Window)
+
         
         Window_x, Window_y = Window.center
 
